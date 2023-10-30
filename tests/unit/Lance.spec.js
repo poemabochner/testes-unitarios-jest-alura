822ed1b5e2ca95ec45d8fa73f80608776529f63c
Poema Bochner <poemabochner8@gmail.com>
2023-10-30 20:22:56 -0300

    testando se o valor emitido corresponde ao valor do input

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -5,5 +5,26 @@ it('não aceita lance com valor menor do que zero', () => {
   const wrapper = mount(Lance)
   const input = wrapper.find('input')
   input.setValue(-100)
-  expect(input).toBeTruthy()
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toBeUndefined()
+})
+
+it('emite um lance quando o valor é maior do que zero', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toHaveLength(1)
+})
+
+it('emite o valor esperado de um lance válido', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  const lance = parseInt(lancesEmitidos[0][0])
+  expect(lance).toBe(100)
 })
