25236d661fad345fe2d63f0b72b4228b455ad9ac
Poema Bochner <poemabochner8@gmail.com>
2023-10-30 20:32:17 -0300

    adicionando describe

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -1,30 +1,32 @@
 import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
 
-it('não aceita lance com valor menor do que zero', () => {
-  const wrapper = mount(Lance)
-  const input = wrapper.find('input')
-  input.setValue(-100)
-  wrapper.trigger('submit')
-  const lancesEmitidos = wrapper.emitted('novo-lance')
-  expect(lancesEmitidos).toBeUndefined()
-})
+describe('Um Lance sem valor mínimo', () => {
+  it('não aceita lance com valor menor do que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(-100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toBeUndefined()
+  })
 
-it('emite um lance quando o valor é maior do que zero', () => {
-  const wrapper = mount(Lance)
-  const input = wrapper.find('input')
-  input.setValue(100)
-  wrapper.trigger('submit')
-  const lancesEmitidos = wrapper.emitted('novo-lance')
-  expect(lancesEmitidos).toHaveLength(1)
-})
+  it('emite um lance quando o valor é maior do que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+  })
 
-it('emite o valor esperado de um lance válido', () => {
-  const wrapper = mount(Lance)
-  const input = wrapper.find('input')
-  input.setValue(100)
-  wrapper.trigger('submit')
-  const lancesEmitidos = wrapper.emitted('novo-lance')
-  const lance = parseInt(lancesEmitidos[0][0])
-  expect(lance).toBe(100)
+  it('emite o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
+  })
 })
