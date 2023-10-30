73cadd3d097c4976c6e8ce9bfb03ba70e9145db6
Poema Bochner <poemabochner8@gmail.com>
2023-10-30 20:00:13 -0300

    avançando com os estudos

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -3,5 +3,7 @@ import { mount } from '@vue/test-utils'
 
 it('não aceita lance com valor menor do que zero', () => {
   const wrapper = mount(Lance)
-  expect(wrapper).toBeTruthy()
+  const input = wrapper.find('input')
+  input.setValue(-100)
+  expect(input).toBeTruthy()
 })
